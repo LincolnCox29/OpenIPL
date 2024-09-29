bb65e286ab5da9e54c6ccd36e907f48f5fb24b79
LincolnCox29 <just.kcgo@gmail.com>
2024-09-29 16:18:41 +1000

    init

---
+++ ImgLib.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#ifndef IMG_LIB_H
+#define IMG_LIB_H
+
+#define MaxComponent(r, g, b) ((r) > (g) ? ((r) > (b) ? (r) : (b)) : ((g) > (b) ? (g) : (b)))
+
+typedef struct
+{
+    int width, height, channels;
+    unsigned char* data;
+} Img;
+
+typedef enum {
+    IMG_LIB_SUCCESS = 0,
+    IMG_LIB_ERROR_LOADING_IMAGE,
+} ImgLibErrorCode;
+
+typedef struct {
+    ImgLibErrorCode code;
+    const char* message;
+} ImgLibErrorInfo;
+
+ImgLibErrorInfo imgToGrayscale(Img* img, float factor);
+ImgLibErrorInfo imgToBlackAndWhite(Img* img, float factor);
+
+#ifdef IMG_LIB_IMPLEMENTATION
+
+ImgLibErrorInfo imgToGrayscale(Img* img, float factor)
+{
+    ImgLibErrorInfo err;
+    if (img->data == NULL)
+    {
+        err.code = IMG_LIB_ERROR_LOADING_IMAGE;
+        err.message = "The specified path may be invalid or the file may not exist. Please check the file path and permissions.";
+        return err;
+    }
+
+    int pIndex = 0;
+    unsigned char maxComponent = NULL;
+
+    for (int y = 0; y < img->height; y++)
+    {
+        for (int x = 0; x < img->width; x++)
+        {
+            pIndex = (y * img->width + x) * img->channels;
+            maxComponent = MaxComponent(img->data[pIndex + 0], img->data[pIndex + 1], img->data[pIndex + 2]);
+            for (int c = 0; c < 3; c++)
+                img->data[pIndex + c] = (int)(maxComponent * factor);
+        }
+    }
+
+    err.code = 0;
+    return err;
+}
+
+ImgLibErrorInfo imgToBlackAndWhite(Img* img, float factor)
+{
+    ImgLibErrorInfo err;
+    if (img->data == NULL)
+    {
+        err.code = IMG_LIB_ERROR_LOADING_IMAGE;
+        err.message = "The specified path may be invalid or the file may not exist. Please check the file path and permissions.";
+        return err;
+    }
+    int brightness;
+    int pIndex = 0;
+
+    for (int y = 0; y < img->height; y++)
+    {
+        for (int x = 0; x < img->width; x++)
+        {
+            pIndex = (y * img->width + x) * img->channels;
+            brightness = (int)(0.299 * img->data[pIndex + 0] + 0.587 * img->data[pIndex + 1] + 0.114 * img->data[pIndex + 2]);
+            if (brightness > (int)(128 * factor))
+                for (int c = 0; c < 3; c++)
+                    img->data[pIndex + c] = 255;
+            else
+                for (int c = 0; c < 3; c++)
+                    img->data[pIndex + c] = 0;
+        }
+    }
+
+    err.code = 0;
+    return err;
+}
+
+#endif
+
+#endif // IMG_LIB_H
---
+++ tests.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#define IMG_LIB_IMPLEMENTATION
+#include "ImgLib.h"
+
+int main() //tests
+{
+    Img* img = malloc(sizeof(Img));
+    img->data = stbi_load("example.png", &img->width, &img->height, &img->channels, 0);
+    ImgLibErrorInfo err = imgToGrayscale(img, 0.1);
+    if (err.code != 0)
+        printf("code: %d msg: %s", err.code, err.message);
+    stbi_write_png("output.png",
+        img->width, img->height, img->channels, img->data, img->width * img->channels);
+    return 0;
+}
