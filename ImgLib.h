95a1f0fdb220d3810dfefda04d7646d308f0b153
LincolnCox29 <just.kcgo@gmail.com>
2024-10-13 20:42:47 +1000

    update imgToMirror

--- ImgLib.h
+++ ImgLib.h
@@ -10,6 +10,7 @@
 #define MIN_COLOR_VALUE 0
 
 #include <stdbool.h>
+#include <math.h>
 
 typedef struct
 {
@@ -408,31 +409,29 @@ ImgLibErrorInfo imgSobelFilter(Img* img, float factor)
 
 ImgLibErrorInfo imgToMirror(Img* img)
 {
-    unsigned char* tampData = imgDataAlloc(img);
     ImgLibErrorInfo err = { IMG_LIB_SUCCESS, NULL };
-    if ((err = imgDataValidation(img->data)).code != IMG_LIB_SUCCESS ||
-        (err = memallocValidation(tampData)).code != IMG_LIB_SUCCESS)
-    {
-        free(tampData);
+    if ((err = imgDataValidation(img->data)).code != IMG_LIB_SUCCESS)
         return err;
-    }
 
     int pIndex = 0;
+    unsigned char tempData;
+    int targetIndex;
 
     for (int y = 0; y < img->height; y++)
     {
-        for (int x = 0; x < img->width; x++)
+        for (int x = 0; x < img->width / 2; x++)
         {
             for (int c = 0; c < img->channels; c++)
             {
-                pIndex = (y * img->width + x) * img->channels;
-                tampData[((y + 1) * img->width - x) * img->channels + c] = img->data[pIndex + c];
+                pIndex = (y * img->width + x) * img->channels + c;
+                targetIndex = (y * img->width + (img->width - 1 - x)) * img->channels + c;
+
+                tempData = img->data[pIndex];
+                img->data[pIndex] = img->data[targetIndex];
+                img->data[targetIndex] = tempData;
             }
         }
     }
-
-    memcpy(img->data, tampData, img->width * img->height * img->channels * sizeof(unsigned char));
-    free(tampData);
 
     return err;
 }
