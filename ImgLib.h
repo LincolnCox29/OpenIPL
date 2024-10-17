af14ce3813b2c745aca0ccc9b4c53d87ce34d473
LincolnCox29 <just.kcgo@gmail.com>
2024-10-17 16:07:21 +1000

    optimization imgSepiaFilter

--- ImgLib.h
+++ ImgLib.h
@@ -264,26 +264,22 @@ ImgLibErrorInfo imgSepiaFilter(Img* img)
         return err;
     }
 
-    int pIndex = 0;
+    const int totalChannels = img->height * img->width * img->channels;
     int newColors[3] = { 0,0,0 };
-    for (int y = 0; y < img->height; y++)
+
+    for (int pIndex = 0; pIndex < totalChannels; pIndex += 3)
     {
-        for (int x = 0; x < img->width; x++)
+        newColors[0] = (int)(
+            (0.393 * img->data[pIndex]) + (0.769 * img->data[pIndex + 1]) + (0.189 * img->data[pIndex + 2]));
+        newColors[1] = (int)(
+            (0.349 * img->data[pIndex]) + (0.686 * img->data[pIndex + 1]) + (0.168 * img->data[pIndex + 2]));
+        newColors[2] = (int)(
+            (0.272 * img->data[pIndex]) + (0.534 * img->data[pIndex + 1]) + (0.131 * img->data[pIndex + 2]));
+
+        for (short i = 0; i < 3; i++)
         {
-            pIndex = (y * img->width + x) * img->channels;
-
-            newColors[0] = (int)(
-                (0.393 * img->data[pIndex]) + (0.769 * img->data[pIndex + 1]) + (0.189 * img->data[pIndex + 2]));
-            newColors[1] = (int)(
-                (0.349 * img->data[pIndex]) + (0.686 * img->data[pIndex + 1]) + (0.168 * img->data[pIndex + 2]));
-            newColors[2] = (int)(
-                (0.272 * img->data[pIndex]) + (0.534 * img->data[pIndex + 1]) + (0.131 * img->data[pIndex + 2]));
-
-            for (short i = 0; i < 3; i++)
-            {
-                clampColorValue(&(newColors[i]));
-                img->data[pIndex + i] = ((unsigned char)newColors[i]);
-            }
+            clampColorValue(&(newColors[i]));
+            img->data[pIndex + i] = ((unsigned char)newColors[i]);
         }
     }
 
