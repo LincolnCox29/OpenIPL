5ccef929362b070247df2f2b33e7bb005d31997c
LincolnCox29 <just.kcgo@gmail.com>
2024-11-10 00:06:34 +1000

    new tests

--- tests.c
+++ tests.c
@@ -2,122 +2,109 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 typedef OpenIPLErrorInfo(*ImgOperationWithFac)(Img*, float);
 typedef OpenIPLErrorInfo(*ImgOperation)(Img*);
+typedef OpenIPLErrorInfo(*ImgOperationWithRGB)(Img*, float, float, float);
+typedef OpenIPLErrorInfo(*ImgOperationWithSize)(Img*, int, int);
+typedef OpenIPLErrorInfo(*ImgOperationWithIterations)(Img*, unsigned);
 
-void absTestWithFac(ImgOperationWithFac func, const float fac, char* outputPath, char* sourcePath);
-void absTest(ImgOperation func, char* outputPath, char* sourcePath);
-void gaussianBlurTest(unsigned iterations, char* outputPath, char* sourcePath);
-void tintTest(float rF, float gF, float bF, char* outputPath, char* sourcePath);
-void bilinearInterpolationTest(int h, int w, char* outputPath, char* sourcePath);
+void testFunction(void* func, int argType, char* outputPath, char* sourcePath, ...);
 
-int main() //tests
+enum 
 {
-    absTestWithFac(imgToGrayscale, 0.8f, "examples\\Grayscale\\Grayscale.png", "examples\\Grayscale\\source.png");
-    absTestWithFac(imgToBlackAndWhite, 0.3f, "examples\\BlackAndWhite\\BlackAndWhite.png", "examples\\BlackAndWhite\\source.png");
-    absTestWithFac(imgAdjustBrightness, 0.7f, "examples\\Brightness\\Brightness.png", "examples\\Brightness\\source.png");
-    absTestWithFac(imgAdjustContrast, 0.7f, "examples\\Contrast\\Contrast.png", "examples\\Contrast\\source.png");
-    absTestWithFac(imgSobelFilter, 0.4f, "examples\\SobelFilter\\SobelFilter.png", "examples\\SobelFilter\\source.png");
+    FUNC_WITH_FLOAT,
+    FUNC_NO_ARGS,
+    FUNC_WITH_RGB,
+    FUNC_WITH_SIZE,
+    FUNC_WITH_ITERATIONS
+};
 
-    absTest(imgSepiaFilter, "examples\\SepiaFilter\\SepiaFilter.png", "examples\\SepiaFilter\\source.png");
-    absTest(imgNegative, "examples\\Negative\\Negative.png", "examples\\Negative\\source.png");
-    absTest(imgToMirror, "examples\\ToMirror\\ToMirror.png", "examples\\ToMirror\\source.png");
-    absTest(imgTurn90, "examples\\Turn90\\Turn90.png", "examples\\Turn90\\source.png");
-    absTest(imgSharpen, "examples\\Sharpen\\Sharpen.png", "examples\\Sharpen\\source.png");
+int main() 
+{
+    testFunction(imgToGrayscale, FUNC_WITH_FLOAT, "examples\\Grayscale\\Grayscale.png", "examples\\Grayscale\\source.png", 0.8f);
+    testFunction(imgToBlackAndWhite, FUNC_WITH_FLOAT, "examples\\BlackAndWhite\\BlackAndWhite.png", "examples\\BlackAndWhite\\source.png", 0.3f);
+    testFunction(imgAdjustBrightness, FUNC_WITH_FLOAT, "examples\\Brightness\\Brightness.png", "examples\\Brightness\\source.png", 0.7f);
+    testFunction(imgAdjustContrast, FUNC_WITH_FLOAT, "examples\\Contrast\\Contrast.png", "examples\\Contrast\\source.png", 0.7f);
+    testFunction(imgSobelFilter, FUNC_WITH_FLOAT, "examples\\SobelFilter\\SobelFilter.png", "examples\\SobelFilter\\source.png", 0.4f);
 
-    bilinearInterpolationTest(256, 200, "examples\\BilinearInterpolation\\BilinearInterpolation.png", "examples\\BilinearInterpolation\\source.png");
+    testFunction(imgSepiaFilter, FUNC_NO_ARGS, "examples\\SepiaFilter\\SepiaFilter.png", "examples\\SepiaFilter\\source.png");
+    testFunction(imgNegative, FUNC_NO_ARGS, "examples\\Negative\\Negative.png", "examples\\Negative\\source.png");
+    testFunction(imgToMirror, FUNC_NO_ARGS, "examples\\ToMirror\\ToMirror.png", "examples\\ToMirror\\source.png");
+    testFunction(imgTurn90, FUNC_NO_ARGS, "examples\\Turn90\\Turn90.png", "examples\\Turn90\\source.png");
+    testFunction(imgSharpen, FUNC_NO_ARGS, "examples\\Sharpen\\Sharpen.png", "examples\\Sharpen\\source.png");
 
-    gaussianBlurTest(50u, "examples\\GaussianBlur\\GaussianBlur.png", "examples\\GaussianBlur\\source.png");
+    testFunction(imgBilinearInterpolation, FUNC_WITH_SIZE, "examples\\BilinearInterpolation\\BilinearInterpolation.png", "examples\\BilinearInterpolation\\source.png", 256, 200);
 
-    tintTest(1.2f, 1.0f, 0.8f, "examples\\Tint\\Tint.png", "examples\\Tint\\source.png");
+    testFunction(imgGaussianBlur, FUNC_WITH_ITERATIONS, "examples\\GaussianBlur\\GaussianBlur.png", "examples\\GaussianBlur\\source.png", 50u);
+
+    testFunction(imgTint, FUNC_WITH_RGB, "examples\\Tint\\Tint.png", "examples\\Tint\\source.png", 1.2f, 1.0f, 0.8f);
 
     return 0;
 }
 
-void absTestWithFac(ImgOperationWithFac func, const float fac, char* outputPath, char* sourcePath)
+void testFunction(void* func, int argType, char* outputPath, char* sourcePath, ...) 
 {
+    va_list args;
+    va_start(args, sourcePath);
+
     clock_t start, end;
     Img* img = loadImg(sourcePath);
+    OpenIPLErrorInfo err;
+
     start = clock();
-    OpenIPLErrorInfo err = func(img, fac);
+
+    switch (argType) 
+    {
+        case FUNC_WITH_FLOAT: 
+        {
+            ImgOperationWithFac operation = (ImgOperationWithFac)func;
+            float fac = (float)va_arg(args, double);
+            err = operation(img, fac);
+            break;
+        }
+        case FUNC_NO_ARGS: 
+        {
+            ImgOperation operation = (ImgOperation)func;
+            err = operation(img);
+            break;
+        }
+        case FUNC_WITH_RGB: 
+        {
+            ImgOperationWithRGB operation = (ImgOperationWithRGB)func;
+            float rF = (float)va_arg(args, double);
+            float gF = (float)va_arg(args, double);
+            float bF = (float)va_arg(args, double);
+            err = operation(img, rF, gF, bF);
+            break;
+        }
+        case FUNC_WITH_SIZE: 
+        {
+            ImgOperationWithSize operation = (ImgOperationWithSize)func;
+            int h = va_arg(args, int);
+            int w = va_arg(args, int);
+            err = operation(img, h, w);
+            break;
+        }
+        case FUNC_WITH_ITERATIONS: 
+        {
+            ImgOperationWithIterations operation = (ImgOperationWithIterations)func;
+            unsigned iterations = va_arg(args, unsigned);
+            err = operation(img, iterations);
+            break;
+        }
+    }
+
     end = clock();
     double timeSpent = (double)(end - start) / CLOCKS_PER_SEC;
     printf("time: %f sec ---> %s\n", timeSpent, outputPath);
+
     if (err.code != 0)
-        printf("code: %d msg: %s", err.code, err.message);
-    writeImg(outputPath, *img);
-    imgFree(img);
-}
+        printf("code: %d msg: %s\n", err.code, err.message);
+    else 
+        writeImg(outputPath, *img);
 
-void absTest(ImgOperation func, char* outputPath, char* sourcePath)
-{
-    clock_t start, end;
-    OpenIPLErrorInfo err;
-    Img* img = loadImg(sourcePath);
-    start = clock();
-    if ((err = func(img)).code != 0)
-    {
-        printf("code: %d msg: %s", err.code, err.message);
-        return;
-    }
-    end = clock();
-    double timeSpent = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("time: %f sec ---> %s\n", timeSpent, outputPath);
-    writeImg(outputPath, *img);
     imgFree(img);
-}
-
-void gaussianBlurTest(unsigned iterations, char* outputPath, char* sourcePath)
-{
-    clock_t start, end;
-    OpenIPLErrorInfo err;
-    Img* img = loadImg(sourcePath);
-    start = clock();
-    if ((err = imgGaussianBlur(img, iterations)).code != 0)
-    {
-        printf("code: %d msg: %s", err.code, err.message);
-        return;
-    }
-    end = clock();
-    double timeSpent = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("time: %f sec ---> %s\n", timeSpent, outputPath);
-    writeImg(outputPath, *img);
-    imgFree(img);
-}
-
-void tintTest(float rF, float gF, float bF, char* outputPath, char* sourcePath)
-{
-    clock_t start, end;
-    OpenIPLErrorInfo err;
-    Img* img = loadImg(sourcePath);
-    start = clock();
-    if ((err = imgTint(img, rF, gF, bF)).code != 0)
-    {
-        printf("code: %d msg: %s", err.code, err.message);
-        return;
-    }
-    end = clock();
-    double timeSpent = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("time: %f sec ---> %s\n", timeSpent, outputPath);
-    writeImg(outputPath, *img);
-    imgFree(img);
-}
-
-void bilinearInterpolationTest(int h, int w, char* outputPath, char* sourcePath)
-{
-    clock_t start, end;
-    OpenIPLErrorInfo err;
-    Img* img = loadImg(sourcePath);
-    start = clock();
-    if ((err = imgBilinearInterpolation(img, h, w)).code != 0)
-    {
-        printf("code: %d msg: %s", err.code, err.message);
-        return;
-    }
-    end = clock();
-    double timeSpent = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("time: %f sec ---> %s\n", timeSpent, outputPath);
-    writeImg(outputPath, *img);
-    imgFree(img);
+    va_end(args);
 }
