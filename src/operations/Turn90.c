9aff6288f3af2551a2a10fc808cb3337419c8cdb
LincolnCox29 <just.kcgo@gmail.com>
2024-11-19 14:15:27 +1000

    Refactoring Turn90.c

--- src/operations/Turn90.c
+++ src/operations/Turn90.c
@@ -1,32 +1,31 @@
 #include "../errors.h"
 #include "../tools.h"
 
+static inline int getPixelIndex(int x, int y, int c, const Img* imgPtr) 
+{
+    return (y * imgPtr->width + x) * imgPtr->channels + c;
+}
+static inline int getTargetIndex(int x, int y, int c, int width, const Img* imgPtr) 
+{
+    return ((x * width) + (width - y - 1)) * imgPtr->channels + c;
+}
+
 OpenIPLErrorInfo imgTurn90(Img* img)
 {
     const int newWidth = img->height;
     const int newHeight = img->width;
+
     unsigned char* rotatedData = (unsigned char*)malloc(newWidth * newHeight * img->channels * sizeof(unsigned char));
+
     OpenIPLErrorInfo err = SUCCESS;
     if ((err = imgDataValidation(img->data)).code != OIPL_SUCCESS ||
         (err = memallocValidation(rotatedData)).code != OIPL_SUCCESS)
         return err;
 
-    int pIndex;
-    int targetIndex;
-
     for (int y = 0; y < img->height; y++)
-    {
         for (int x = 0; x < img->width; x++)
-        {
             for (int c = 0; c < img->channels; c++)
-            {
-                pIndex = (y * img->width + x) * img->channels + c;
-                targetIndex = ((x * newWidth) + (newWidth - y - 1)) * img->channels + c;
-
-                rotatedData[targetIndex] = img->data[pIndex];
-            }
-        }
-    }
+                rotatedData[getTargetIndex(x, y, c, newWidth, img)] = img->data[getPixelIndex(x, y, c, img)];
 
     free(img->data);
     img->data = rotatedData;
