b251533f7da602cce4b25fed1bfae1bd1cf22e6d
LincolnCox29 <just.kcgo@gmail.com>
2024-11-28 20:38:36 +1000

    optimization sharpen.c

--- src/operations/sharpen.c
+++ src/operations/sharpen.c
@@ -4,51 +4,54 @@
 
 OpenIPLErrorInfo imgSharpen(Img* img)
 {
-    unsigned char* currentData = img->data;
-    unsigned char* sharpenedData = imgDataAlloc(img);
-
-    if (sharpenedData == NULL)
-    {
-        free(sharpenedData);
-        return FAILED_MEMORY_ALLOCATION;
-    }
     if (img->data == NULL)
         return ERROR_LOADING_IMAGE;
 
-    const int weights[3][3] = {
+    unsigned char* sharpenedData = imgDataAlloc(img);
+    if (sharpenedData == NULL)
+        return FAILED_MEMORY_ALLOCATION;
+
+    const int weights[3][3] = 
+    {
         { 0, -1,  0},
         {-1,  5, -1},
         { 0, -1,  0}
     };
-    const int weightSum = 1;
 
-    for (int y = 1; y < img->height - 1; y++)
+    const int width = img->width;
+    const int height = img->height;
+    const int channels = img->channels;
+
+    for (int y = 1; y < height - 1; y++)
     {
-        for (int x = 1; x < img->width - 1; x++)
+        for (int x = 1; x < width - 1; x++)
         {
-            int pIndex = (y * img->width + x) * img->channels;
+            int pIndex = (y * width + x) * channels;
 
-            for (int c = 0; c < img->channels; c++)
+            for (int c = 0; c < channels; c++)
             {
                 int sum = 0;
 
-                for (int ky = -1; ky <= 1; ky++)
-                {
-                    for (int kx = -1; kx <= 1; kx++)
-                    {
-                        int neighborIndex = ((y + ky) * img->width + (x + kx)) * img->channels + c;
-                        sum += currentData[neighborIndex] * weights[ky + 1][kx + 1];
-                    }
-                }
+                sum += img->data[((y - 1) * width + (x - 1)) * channels + c] * weights[0][0];
+                sum += img->data[((y - 1) * width + (x)) * channels + c] * weights[0][1];
+                sum += img->data[((y - 1) * width + (x + 1)) * channels + c] * weights[0][2];
 
-                sum = sum / weightSum;
+                sum += img->data[((y)*width + (x - 1)) * channels + c] * weights[1][0];
+                sum += img->data[((y)*width + (x)) * channels + c] * weights[1][1];
+                sum += img->data[((y)*width + (x + 1)) * channels + c] * weights[1][2];
+
+                sum += img->data[((y + 1) * width + (x - 1)) * channels + c] * weights[2][0];
+                sum += img->data[((y + 1) * width + (x)) * channels + c] * weights[2][1];
+                sum += img->data[((y + 1) * width + (x + 1)) * channels + c] * weights[2][2];
+
                 clampColorValue(&sum);
                 sharpenedData[pIndex + c] = (unsigned char)sum;
             }
         }
     }
 
-    memcpy(img->data, sharpenedData, img->width * img->height * img->channels);
+    memcpy(img->data, sharpenedData, width * height * channels);
     free(sharpenedData);
+
     return SUCCESS;
 }
