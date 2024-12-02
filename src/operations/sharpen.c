54a377c6f3a6aaf095442cc5482c21007e7a75b2
LincolnCox29 <just.kcgo@gmail.com>
2024-12-02 21:24:58 +1000

    optimization sharpen.c

--- src/operations/sharpen.c
+++ src/operations/sharpen.c
@@ -22,27 +22,30 @@ OpenIPLErrorInfo imgSharpen(Img* img)
     const int height = img->height;
     const int channels = img->channels;
 
+    int sum;
+    int pIndex;
+
     for (int y = 1; y < height - 1; y++)
     {
         for (int x = 1; x < width - 1; x++)
         {
-            int pIndex = (y * width + x) * channels;
+            pIndex = (y * width + x) * channels;
 
             for (int c = 0; c < channels; c++)
             {
-                int sum = 0;
+                sum = (
+                    img->data[((y - 1) * width + (x - 1)) * channels + c] * weights[0][0] +
+                    img->data[((y - 1) * width + (x)) * channels + c] * weights[0][1] +
+                    img->data[((y - 1) * width + (x + 1)) * channels + c] * weights[0][2] +
 
-                sum += img->data[((y - 1) * width + (x - 1)) * channels + c] * weights[0][0];
-                sum += img->data[((y - 1) * width + (x)) * channels + c] * weights[0][1];
-                sum += img->data[((y - 1) * width + (x + 1)) * channels + c] * weights[0][2];
+                    img->data[((y)*width + (x - 1)) * channels + c] * weights[1][0] +
+                    img->data[((y)*width + (x)) * channels + c] * weights[1][1] +
+                    img->data[((y)*width + (x + 1)) * channels + c] * weights[1][2] +
 
-                sum += img->data[((y)*width + (x - 1)) * channels + c] * weights[1][0];
-                sum += img->data[((y)*width + (x)) * channels + c] * weights[1][1];
-                sum += img->data[((y)*width + (x + 1)) * channels + c] * weights[1][2];
-
-                sum += img->data[((y + 1) * width + (x - 1)) * channels + c] * weights[2][0];
-                sum += img->data[((y + 1) * width + (x)) * channels + c] * weights[2][1];
-                sum += img->data[((y + 1) * width + (x + 1)) * channels + c] * weights[2][2];
+                    img->data[((y + 1) * width + (x - 1)) * channels + c] * weights[2][0] +
+                    img->data[((y + 1) * width + (x)) * channels + c] * weights[2][1] +
+                    img->data[((y + 1) * width + (x + 1)) * channels + c] * weights[2][2]
+                    );
 
                 clampColorValue(&sum);
                 sharpenedData[pIndex + c] = (unsigned char)sum;
